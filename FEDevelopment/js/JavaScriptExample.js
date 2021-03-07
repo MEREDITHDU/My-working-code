9d0727b91a1f8cd913bf6320d8d0b547c428ba08
QILIN DU <1049467313@qq.com>
2021-03-07 21:13:35 +0100

    Update JavaScriptExample.js

--- FEDevelopment/js/JavaScriptExample.js
+++ FEDevelopment/js/JavaScriptExample.js
@@ -1,7 +1,14 @@
-(function ($) {
 
 	'use strict';
 
-	
+	function ageInDays() {
+	let BirthYear=prompt('Which year were you born');
+	let AgeInDays=(2021-BirthYear)*365;
+	let h1=document.createElement('h1');
+	let Answer=document.createTextNode('You are'+AgeInDays+'Days old');
+	h1.setAttribute('id','ageInDays');
+	h1.appendChild(Answer);
+	document.getElementById('id').appendChild('h1');
+}
+// ageInDays();
 
-})
